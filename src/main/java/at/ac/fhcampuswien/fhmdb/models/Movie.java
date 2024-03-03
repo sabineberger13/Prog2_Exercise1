c019966ddbdbfdd9b77d6e5662f65f5689f256c1
sabineberger13 <sabineberger1301@gmail.com>
2024-03-03 17:20:21 +0100

    Movies, Genres

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,16 +1,20 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
-    private String title;
-    private String description;
-    // TODO add more properties here
+    private final String title;
+    private final String description;
 
-    public Movie(String title, String description) {
+    // TODO add more properties here#
+
+    private final List<Genre> genres;
+    public Movie(String title, String description, List<Genre> genres) {
         this.title = title;
         this.description = description;
+        this.genres = genres;
     }
 
     public String getTitle() {
@@ -21,9 +25,22 @@ public class Movie {
         return description;
     }
 
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
         // TODO add some dummy data here
+        movies.add(new Movie(
+                "JOKER",
+                "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure." ,
+                Arrays.asList(Genre.DRAMA, Genre.THRILLER, Genre.HORROR)));
+
+        movies.add(new Movie(
+                "A Star is born",
+                "Seasoned musician Jackson Maine discovers — and falls in love with — struggling artist Ally. She has just about given up on her dream to make it big as a singer — until Jack coaxes her into the spotlight. But even as Ally's career takes off, the personal side of their relationship is breaking down, as Jack fights an ongoing battle with his own internal demons.",
+                Arrays.asList(Genre.COMEDY, Genre.FAMILY, Genre.ANIMATION)));
 
         return movies;
     }
---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/SortedState.java
@@ -0,0 +1,12 @@
+package at.ac.fhcampuswien.fhmdb.models;
+
+public enum SortedState {
+
+    ASCENDING,
+
+    DESCENDING,
+
+    NONE
+
+
+}
